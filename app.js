a550c9a276c595cb27d0f1e0389aa51bca299dc9
Andrew Park <andresparque@gmail.com>
2017-10-23 18:30:43 +0000

    Added new view files

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 // app.use(methodOverride('_method'));
 app.use(cookieParser());
 
+app.locals.thumbnail = 1;
+app.locals.title = 2;
+app.locals.author = 3;
+
 //Sample endpoint
 app.get("/", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+  res.render("index")
 });
 
 //Listening to the appropriate PORT
